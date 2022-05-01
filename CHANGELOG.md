c4ce98263441f42417ce2225e263f50b6f53cee7
f3ath <karapetov@gmail.com>
2022-04-30 19:54:20 -0700

    v0.0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,12 @@
+## [0.0.5] - 2022-04-30
+### Changed
+- Filesystem migrations source moved to its own package
+
 ## [0.0.4] - 2022-04-30
 ### Added
 - Migration equality operator
 
 ## [0.0.3] - 2022-04-30
-## [0.0.2] - 2022-04-30
 ### Removed
 - Migration.summary
 
@@ -11,7 +14,7 @@
 ### Added
 - Initial version
 
+[0.0.5]: https://github.com/f3ath/migrant/compare/0.0.4...0.0.5
 [0.0.4]: https://github.com/f3ath/migrant/compare/0.0.3...0.0.4
 [0.0.3]: https://github.com/f3ath/migrant/compare/0.0.2...0.0.3
-[0.0.2]: https://github.com/f3ath/migrant/compare/0.0.1...0.0.2
 [0.0.1]: https://github.com/f3ath/migrant/releases/tag/0.0.1
--- test/functional_test.dart
+++ test/functional_test.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:migrant/migrant.dart';
 import 'package:migrant/testing.dart';
+import 'package:migrant_source_fs/migrant_source_fs.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -43,65 +44,4 @@ void main() {
       ]));
     }, throwsStateError);
   });
-}
-
-/// Reads migrations from a local filesystem.
-class LocalDirectory extends MigrationSource {
-  LocalDirectory(this._dir, this._format);
-
-  final Directory _dir;
-  final FileNameFormat _format;
-
-  Stream<Migration> read({String? afterVersion = null}) async* {
-    final entries = await _dir.list().toList();
-    final matchedFiles = entries
-        .whereType<File>()
-        .map((file) {
-          final name = file.uri.pathSegments.last;
-          final match = _format.match(name);
-          if (match == null) return null;
-          return MatchedFile(file, match);
-        })
-        .whereType<MatchedFile>()
-        .where((matchedFile) =>
-            afterVersion == null ||
-            matchedFile.version.compareTo(afterVersion) > 0)
-        .toList();
-    matchedFiles.sort();
-
-    for (final file in matchedFiles) {
-      yield await file.toMigration();
-    }
-  }
-}
-
-class MatchedFile implements Comparable<MatchedFile> {
-  MatchedFile(this._file, this.version);
-
-  final File _file;
-
-  final String version;
-
-  @override
-  int compareTo(MatchedFile other) => version.compareTo(other.version);
-
-  Future<Migration> toMigration() async =>
-      Migration(version, await _file.readAsString());
-}
-
-/// Defines the format of the file name.
-class FileNameFormat {
-  FileNameFormat(this._versionFormat, {String extension = '.sql'})
-      : _extension = extension;
-
-  final RegExp _versionFormat;
-
-  final String _extension;
-
-  /// Matches the [name] against the format,
-  /// returns a [FileNameMatch] if successful.
-  String? match(String name) {
-    if (!name.endsWith(_extension)) return null;
-    return _versionFormat.matchAsPrefix(name)?.group(0);
-  }
 }
